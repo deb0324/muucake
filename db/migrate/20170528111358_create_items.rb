133052d84aef18e052ec9d84472507f8dd0831d0
Debbie Tseng <debbie_wei0324@hotmail.com>
2017-05-29 15:28:13 +0800

    set up model associations

--- db/migrate/20170528111358_create_items.rb
+++ db/migrate/20170528111358_create_items.rb
@@ -2,10 +2,10 @@ class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
 
-      t.string  :product_id
+      t.string  :product
       t.integer :price
       t.integer :quantity
-      t.integer :order_id
+      t.references :order, foreign_key: true
 
       t.timestamps
     end
---
+++ db/schema.rb
@@ -0,0 +1,47 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170528111434) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "product"
+    t.integer  "price"
+    t.integer  "quantity"
+    t.integer  "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "receiver"
+    t.string   "phone"
+    t.string   "method"
+    t.date     "pickup_date"
+    t.time     "pickup_time"
+    t.string   "address"
+    t.integer  "fee"
+    t.integer  "lala_fee"
+    t.text     "note"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name_chin"
+    t.string   "name_eng"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
